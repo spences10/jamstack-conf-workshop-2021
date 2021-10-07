5dac4b4b77a668349a64831e94888eae6dde9fcf
spences10 <spences10apps@gmail.com>
2021-10-07 20:47:29 +0100

    feat: :sparkles: add pages and page endpoint and page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,17 @@
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const res = await fetch('/pages.json')
+    if (res.ok) {
+      const pages = await res.json()
+      return {
+        props: pages,
+      }
+    }
+  }
+</script>
+
 <script>
   import Nav from '$lib/nav.svelte'
-import ThemeSwitch from '$lib/theme-switch.svelte'
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
   import '../app.css'
@@ -8,9 +19,11 @@ import ThemeSwitch from '$lib/theme-switch.svelte'
   onMount(async () => {
     themeChange(false)
   })
+
+  export let pages
 </script>
 
-<Nav/>
+<Nav {pages} />
 <main class="container max-w-xl mx-auto px-4">
   <slot />
 </main>
