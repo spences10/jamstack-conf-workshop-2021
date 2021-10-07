ce9dd5a9430b2e6a1e6f5893701c93438f224544
spences10 <spences10apps@gmail.com>
2021-10-07 20:33:46 +0100

    feat: :sparkles: Add nav bar and theme switch

---
+++ src/lib/nav.svelte
@@ -0,0 +1,19 @@
+<script>
+  import ThemeSwitch from './theme-switch.svelte'
+</script>
+
+<div
+  class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box sticky top-0 z-10"
+>
+  <div class="px-2 mx-2 navbar-start">
+    <span class="text-lg font-bold"> Jamstack Conf Blog </span>
+  </div>
+  <div class="hidden px-2 mx-2 navbar-center lg:flex">
+    <div class="flex items-stretch">
+      <a href="/" class="btn btn-ghost btn-sm rounded-btn"> Home </a>
+    </div>
+  </div>
+  <div class="navbar-end">
+    <ThemeSwitch />
+  </div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,16 @@
 <script>
+  import Nav from '$lib/nav.svelte'
+import ThemeSwitch from '$lib/theme-switch.svelte'
+  import { onMount } from 'svelte'
+  import { themeChange } from 'theme-change'
   import '../app.css'
+
+  onMount(async () => {
+    themeChange(false)
+  })
 </script>
 
+<Nav/>
 <main class="container max-w-xl mx-auto px-4">
   <slot />
 </main>
