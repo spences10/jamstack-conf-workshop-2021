db10260a7956ce9f426ac5ccd365d84c703d8c61
spences10 <spences10apps@gmail.com>
2021-10-07 18:43:48 +0100

    feat: :sparkles: init

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch('/posts.json')
+    if (res.ok) {
+      const posts = await res.json()
+      return {
+        props: posts,
+      }
+    }
+  }
+</script>
+
+<script>
+  export let posts
+</script>
+
+<pre>{JSON.stringify(posts, null, 2)}</pre>
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+  documentation
+</p>
