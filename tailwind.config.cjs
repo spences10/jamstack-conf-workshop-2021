41236147b12be5c03fd1a799abd4873a3c481dda
spences10 <spences10apps@gmail.com>
2021-10-07 19:12:35 +0100

    style: :lipstick: style index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 const config = {
-  mode: "jit",
-  purge: ["./src/**/*.{html,js,svelte,ts}"],
+  mode: 'jit',
+  purge: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {}
+    extend: {},
   },
 
-  plugins: []
-};
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+}
 
-module.exports = config;
+module.exports = config
