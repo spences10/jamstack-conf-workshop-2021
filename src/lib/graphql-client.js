c8fe976c9d0d6524b1a3be03b13544f703735599
spences10 <spences10apps@gmail.com>
2021-10-08 17:01:16 +0100

    feat: :sparkles: Add client for using mutation

--- src/lib/graphql-client.js
+++ src/lib/graphql-client.js
@@ -1,4 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 const GRAPHQL_ENDPOINT = process.env['GRAPHQL_ENDPOINT']
+const POST_TOKEN = process.env['POST_TOKEN']
 
 export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
+
+export const postClient = new GraphQLClient(GRAPHQL_ENDPOINT, {
+  headers: { Authorization: `Bearer ${POST_TOKEN}` },
+})
